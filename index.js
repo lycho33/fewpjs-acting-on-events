1b85d5a79f8ed133c2456db847cd78827ceb1c50
lycho33 <cho.lyds@gmail.com>
2021-08-24 22:38:14 -0400

    commit tree 7cfc2a0a924288b10f5dc4c48666f7c862d4fba4

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
-// Your code here
+// // Your code here
+// let dodger = document.getElementById("dodger");
+
+// function moveDodgerLeft() {
+//     let leftNumbers = dodger.style.left.replace("px", "");
+//     let left = parseInt(leftNumbers, 10);
+  
+//     if (left > 0) {
+//       dodger.style.left = `${left - 1}px`;
+//     }
+//   } 
+
+// function moveDodgerRight() {
+//     let leftNumbers = dodger.style.left.replace("px", "");
+//     let left = parseInt(leftNumbers, 10);
+
+//     if (left > 0) {
+//         dodger.style.left = `${left + 1}px`;
+//     }
+// }
+
+// //these functions will only work with the "left key" and not the right key yet 
+// document.addEventListener("keydown", function(e) {
+//     if (e.key === "ArrowLeft") {
+//       moveDodgerLeft();
+//     }
+//   }); 
+
+
+
+let dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+function moveDodgerRight() {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+
+  if (left < 0){
+    dodger.style.left = `${left + 1}px`
+  }
+}
+
+document.addEventListener("keydown", function(e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  } else if (e.key === 'ArrowRight'){
+    moveDodgerRight()
+  }
+});
